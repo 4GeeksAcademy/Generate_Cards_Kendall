61793def28a8b0ab82846aad2237b97b28744496
Kenube21 <kendallsh51@gmail.com>
2026-04-20 05:02:13 +0000

    resolved

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = document.querySelectorAll('.suits');
+  const number = document.querySelector('#number')
+  
+  const listSuits = ["♦" ,"♥", "♠", "♣"];
+  let randomSuits = listSuits[Math.floor(Math.random() * 4)];
+
+  const listNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomNumber = listNumber[Math.floor(Math.random() * 13)];
+
+  number.innerText = randomNumber;
+  
+  suits.forEach(elem => {
+    if (randomSuits === "♦" || randomSuits === "♥") {
+    suits.forEach(elem => {
+      elem.style.color = "red";
+    });
+  }
+    elem.innerText = randomSuits;
+  })
+
 };
